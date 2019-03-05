124ad2a6d8d08b099c51ae0c4a5bda64780c65e8
Nicholas Soetaert <32920854+NickSoetaert@users.noreply.github.com>
2019-03-05 13:48:07 -0800

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -1,32 +1,16 @@
 public class Blackjack{
     public static void main(String[] args){
 
-        //Todo: 
-        //Insurance
-        //try-catch blocks for bad input
-        //ascii-art cards?
-
-        //For debug/showing off split abilities. Make sure to uncomment special Deck constructor
-        /*
-        Card k = new Card(Suit.HEARTS, Value.TWO);
-        Card q = new Card(Suit.CLUBS, Value.ACE);
-        Deck d = new Deck(k, q);
-        */
-
         Player p = new Player();
         Dealer dealer = new Dealer();
 
         while(p.getChips() > 0){
         
             Deck d = new Deck();
-
             p.bet(d);
 
             dealer.initHand(d);
             dealer.printHiddenHand();
-
-            //if hiddenhand == ace
-            //player.promptInsurance()
 
             p.play(d);
             dealer.playHand(d);
